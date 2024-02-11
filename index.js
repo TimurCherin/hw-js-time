b734eba354ce63052b380b8333c8c8493eb43586
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-02-11 16:39:39 +0200

    s

---
+++ index.js
@@ -0,0 +1,33 @@
+const minutesTimer = document.querySelector(".minutes-timer")
+const timer = document.querySelector(".timer")
+const btn = document.querySelector(".btn")
+let time = 60;
+setInterval(countMinutes, 60000)
+function countMinutes(){
+    if(time !== 0){
+        time--;
+    }
+    minutesTimer.textContent = `minutes: ${time}`
+    if(time === 30){
+        alert("залишилось менше половини часу")
+    }
+}
+let timerTime = 30
+setInterval(countSeconds, 200)
+function countSeconds(){
+    if(timerTime !== 0){
+        timerTime--;
+    }
+    timer.textContent = timerTime
+    if(timerTime === 10){
+        timer.classList.add("animation")
+    } 
+    if(timerTime === 0){
+        btn.classList.remove("hide")
+    } 
+}
+btn.addEventListener("click", update)
+function update(){
+    timerTime = 30
+    timer.textContent = timerTime
+}
